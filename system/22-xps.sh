91b3eb50e1cefbc15348ad95e28068780047753d
alexhulbert <alex@alexhulbert.com>
2026-03-01 17:47:37 -0500

    Add ipu6 camera support

--- system/22-xps.sh
+++ system/22-xps.sh
@@ -8,3 +8,15 @@ SystemdEnable interception-tools /usr/lib/systemd/system/udevmon.service
 CopyFileTo /udevmon.yaml /etc/interception/udevmon.yaml
 CopyFileTo /copilot.hwdb /etc/udev/hwdb.d/20-copilot.hwdb
 CopyFileTo /copilot2ctrl /usr/local/bin/copilot2ctrl 755
+
+# ipu6 camera support
+AddPackage libcamera                     # A complex camera support library for Linux, Android, and ChromeOS
+AddPackage pipewire-libcamera            # Low-latency audio/video router and processor - Libcamera support
+AddPackage v4l2loopback-utils            # v4l2-loopback device – utilities only
+AddPackage libcamera-ipa                 # A complex camera support library for Linux, Android, and ChromeOS - signed IPA
+AddPackage libcamera-tools               # A complex camera support library for Linux, Android, and ChromeOS - tools
+AddPackage gst-plugin-libcamera          # Multimedia graph framework - libcamera plugin
+AddPackage --foreign intel-ipu6-dkms-git # Intel IPU6 camera drivers
+cat <<EOF >|"$(CreateFile '/etc/udev/rules.d/camera.rules')"
+KERNEL=="udmabuf", TAG+="uaccess"
+EOF
